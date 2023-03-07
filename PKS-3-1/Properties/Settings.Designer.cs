301c74b4368c1e0fbd12cfb691469dc10fc83615
Andrew Kryazhev <accumhelp@ya.ru>
2023-03-07 11:11:36 +0300

    07-03-2023

--- PKS-3-1/Properties/Settings.Designer.cs
+++ PKS-3-1/Properties/Settings.Designer.cs
@@ -50,8 +50,7 @@ namespace PKS_3_1.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\\Users\\Admin-13\\Documents\\Arhive." +
-            "accdb")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\\Arhive.accdb")]
         public string ArhiveConnectionString {
             get {
                 return ((string)(this["ArhiveConnectionString"]));
